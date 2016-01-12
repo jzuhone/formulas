5561433d4c0d10fe33fb28c7a88bc35e88a4161a
John ZuHone <jzuhone@gmail.com>
2016-01-12 10:54:09 -0500

    More work on constants

--- formulas/constants.py
+++ formulas/constants.py
@@ -1,14 +1,29 @@
 from sympy.core.numbers import Pi
 from formulas.base import FormulaConstant
-import yt.utilities.physical_constants as pc
+try:
+    import yt.utilities.physical_constants as yt_pc
+except ImportError:
+    yt_pc = None
+try:
+    import astropy.constants as ap_pc
+except ImportError:
+    ap_pc = None
 
 pi = Pi()
 
+yt_map = {"m_e":"me","m_p":"mp","m_h":"mh","k_B":"kboltz"}
+ap_map = {}
+
 class PhysicalConstants(object):
-    def __init__(self, constants):
+    def __init__(self, constants, map):
         self.constants = constants
+        self.map = map
 
     def __getattr__(self, item):
-        return FormulaConstant(item, getattr(self.constants,item))
+        const = self.map.get(item, item)
+        return FormulaConstant(item, getattr(self.constants, const))
 
-yt_constants = PhysicalConstants(pc)
+if yt_pc is not None:
+    yt_constants = PhysicalConstants(yt_pc, yt_map)
+if ap_pc is not None:
+    ap_constants = PhysicalConstants(ap_pc, ap_map)
