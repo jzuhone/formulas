d1c76ec7b17d9ec1ca208676ff6c9d4872297cec
John ZuHone <jzuhone@gmail.com>
2019-08-15 14:43:40 -0400

    Fix this

--- formulas/physics.py
+++ formulas/physics.py
@@ -1,9 +1,13 @@
-from formulas.general import gaussian
 from formulas.base import Formula1D
 from sympy import pi, exp, Rational, sqrt, symbols
 
+
 def maxwellian_velocity(v="v", A="A", sigma="sigma"):
-    return gaussian(x=v, A=A, sigma=sigma, mu=0)
+    v, A, sigma = symbols((v, A, sigma))
+    params = [A, sigma]
+    f = A*exp(-Rational(1,2)*((v/sigma)**2))/(sigma*sqrt(2*pi))
+    return Formula1D(f, v, params)
+
 
 def maxwellian_speed(v="v", A="A", sigma="sigma"):
     v, A, sigma = symbols((v, A, sigma))
