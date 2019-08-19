b84817a29450670b848c1584eadca93fcd8d66c7
John ZuHone <jzuhone@gmail.com>
2019-08-18 23:06:43 -0400

    Throw an error here

--- formulas/radial_profiles.py
+++ formulas/radial_profiles.py
@@ -369,6 +369,8 @@ def rescale_profile_by_mass(profile, param, mass, radius):
     rho = profile.copy()
     values = {}
     for n, p in profile.param_values.items():
+        if p is None:
+            raise RuntimeError("The parameter %s does not have a value!" % n)
         if n == param:
             # Set the density parameter to change to unity
             values[n] = 1.0
